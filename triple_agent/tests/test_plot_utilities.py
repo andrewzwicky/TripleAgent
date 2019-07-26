aaf6f5587b3122128af29948d78202a55bcce9bc
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-07-25 19:12:16 -0500

    Update test_plot_utilities.py
    Removing dependency on matplotlib for testing plot_utilities

--- triple_agent/tests/test_plot_utilities.py
+++ triple_agent/tests/test_plot_utilities.py
@@ -4,8 +4,8 @@ import pytest
 
 from triple_agent.reports.plot_utilities import create_data_dictionaries
 from triple_agent.replays.get_parsed_replays import get_parsed_replays
-from triple_agent.reports.action_tests import _at_rates_excluding_difficults
 from triple_agent.utilities.action_tests import ActionTest
+from triple_agent.utilities.timeline import TimelineCategory
 
 TEST_GAME_UUIDS = [
     "8uf6pUK7TFegBD8Cbr2qMw",
@@ -23,31 +23,40 @@ TEST_GAME_UUIDS = [
 
 TEST_GAMES = get_parsed_replays(lambda game: game.uuid in TEST_GAME_UUIDS)
 
+# This is a generic test query so that dependencies from
+# other modules aren't brought into this module.
+def TEST_AT_COUNT(games, data_dictionary):
+    for game in games:
+        for event in game.timeline:
+            if event.category & TimelineCategory.ActionTest:
+                data_dictionary[event.action_test] += 1
+
+
 CREATE_DATA_DICTIONARY_TEST_CASES = [
     (
-        _at_rates_excluding_difficults,
+        TEST_AT_COUNT,
         None,
         Counter(
             {
                 ActionTest.Green: 14,
                 ActionTest.White: 34,
-                ActionTest.Red: 1,
+                ActionTest.Red: 2,
                 ActionTest.Ignored: 2,
                 ActionTest.Canceled: 1,
             }
         ),
         Counter(
             {
-                ActionTest.Green: 14 / 52,
-                ActionTest.White: 34 / 52,
-                ActionTest.Red: 1 / 52,
-                ActionTest.Ignored: 2 / 52,
-                ActionTest.Canceled: 1 / 52,
+                ActionTest.Green: 14 / 53,
+                ActionTest.White: 34 / 53,
+                ActionTest.Red: 2 / 53,
+                ActionTest.Ignored: 2 / 53,
+                ActionTest.Canceled: 1 / 53,
             }
         ),
     ),
     (
-        _at_rates_excluding_difficults,
+        TEST_AT_COUNT,
         lambda x: x.spy,
         defaultdict(
             Counter,
@@ -59,7 +68,7 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                     {
                         ActionTest.Green: 7,
                         ActionTest.White: 21,
-                        ActionTest.Red: 1,
+                        ActionTest.Red: 2,
                         ActionTest.Canceled: 1,
                     }
                 ),
@@ -77,17 +86,17 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                 ),
                 "Max Edward Snax": Counter(
                     {
-                        ActionTest.Green: 7 / 30,
-                        ActionTest.White: 21 / 30,
-                        ActionTest.Red: 1 / 30,
-                        ActionTest.Canceled: 1 / 30,
+                        ActionTest.Green: 7 / 31,
+                        ActionTest.White: 21 / 31,
+                        ActionTest.Red: 2 / 31,
+                        ActionTest.Canceled: 1 / 31,
                     }
                 ),
             },
         ),
     ),
     (
-        _at_rates_excluding_difficults,
+        TEST_AT_COUNT,
         lambda x: x.sniper,
         defaultdict(
             Counter,
@@ -99,7 +108,7 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                     {
                         ActionTest.Green: 7,
                         ActionTest.White: 21,
-                        ActionTest.Red: 1,
+                        ActionTest.Red: 2,
                         ActionTest.Canceled: 1,
                     }
                 ),
@@ -117,17 +126,17 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                 ),
                 "Calvin Schoolidge": Counter(
                     {
-                        ActionTest.Green: 7 / 30,
-                        ActionTest.White: 21 / 30,
-                        ActionTest.Red: 1 / 30,
-                        ActionTest.Canceled: 1 / 30,
+                        ActionTest.Green: 7 / 31,
+                        ActionTest.White: 21 / 31,
+                        ActionTest.Red: 2 / 31,
+                        ActionTest.Canceled: 1 / 31,
                     }
                 ),
             },
         ),
     ),
     (
-        _at_rates_excluding_difficults,
+        TEST_AT_COUNT,
         lambda x: x.venue,
         defaultdict(
             Counter,
@@ -144,6 +153,7 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                         ActionTest.White: 17,
                         ActionTest.Ignored: 1,
                         ActionTest.Canceled: 1,
+                        ActionTest.Red: 1,
                     }
                 ),
                 "Ballroom": Counter({ActionTest.Green: 1, ActionTest.White: 3}),
@@ -168,10 +178,11 @@ CREATE_DATA_DICTIONARY_TEST_CASES = [
                 ),
                 "Gallery": Counter(
                     {
-                        ActionTest.Green: 5 / 24,
-                        ActionTest.White: 17 / 24,
-                        ActionTest.Ignored: 1 / 24,
-                        ActionTest.Canceled: 1 / 24,
+                        ActionTest.Green: 5 / 25,
+                        ActionTest.White: 17 / 25,
+                        ActionTest.Ignored: 1 / 25,
+                        ActionTest.Canceled: 1 / 25,
+                        ActionTest.Red: 1 / 25,
                     }
                 ),
                 "Ballroom": Counter({ActionTest.Green: 1 / 4, ActionTest.White: 3 / 4}),
