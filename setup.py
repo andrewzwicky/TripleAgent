8da553630aeb30647b199e0e7eb0103cf8fc670d
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-07-31 13:03:35 -0500

    Adding pylint and black formatter to build steps.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
             "pytesseract>=0.2.6",
             "numpy>=1.16.3",
         ],
-        "test": ["pytest", "opencv-python>=4.1.0.25", "pytest-cov"],
+        "test": ["pytest", "opencv-python>=4.1.0.25", "pytest-cov", "pylint", "black"],
     },
     setup_requires=["pytest-runner>=4.4"],
     classifiers=[
