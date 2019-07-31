9a1ec6daee35a3ce016ec95f81b43eb6f0d9217d
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-07-31 13:49:11 -0500

    Update get_parsed_replays.py
    Removed duplicate function

--- triple_agent/replays/get_parsed_replays.py
+++ triple_agent/replays/get_parsed_replays.py
@@ -1,24 +1,17 @@
 import os
 from multiprocessing import Pool
-from typing import List
+from typing import List, Optional
 
 from triple_agent.utilities.paths import REPLAY_PICKLE_FOLDER
 from triple_agent.utilities.game import game_unpickle, Game
-
-
-def unpickle_game(replay_pickle_file: str) -> Game:
-    unpickled_game = game_unpickle(
-        os.path.join(REPLAY_PICKLE_FOLDER, replay_pickle_file)
-    )
-    if unpickled_game is not None:
-        return unpickled_game
 
 
 def get_parsed_replays(game_filter) -> List[Game]:
     pool = Pool(processes=4)
 
     try:
-        unfiltered_game_list = pool.map(unpickle_game, os.listdir(REPLAY_PICKLE_FOLDER))
+        replay_pickle_paths = [os.path.join(REPLAY_PICKLE_FOLDER, f) for f in os.listdir(REPLAY_PICKLE_FOLDER)]
+        unfiltered_game_list = pool.map(game_unpickle, replay_pickle_paths)
         filtered_game_list = list(filter(game_filter, unfiltered_game_list))
     finally:
         pool.close()
