3936f1b5f2387c41fc710ec075092c4d9aa28259
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-08-05 17:10:34 -0500

    Update parse_game_timelines_parallel.py
    Refactoring code to split into many portions.  Still needs thorough testing.

--- triple_agent/timeline/parse_game_timelines_parallel.py
+++ triple_agent/timeline/parse_game_timelines_parallel.py
@@ -22,6 +22,8 @@ from triple_agent.utilities.game import Game, get_game_expected_pkl
 from triple_agent.utilities.paths import LONG_FILE_HEADER, PICKLE_ISOLATION, PARSE_LOG
 from triple_agent.utilities.timeline import Timeline
 
+NUM_WORKER_THREADS = 2
+
 
 def get_app_handles() -> Tuple[Optional[int], Optional[int]]:
     _pycharm_handle = None
@@ -34,7 +36,7 @@ def get_app_handles() -> Tuple[Optional[int], Optional[int]]:
     def foreach_window(hwnd, _):
         nonlocal _pycharm_handle
         nonlocal _spy_party_handle
-        title = window__get_title(hwnd)
+        title = window_get_title(hwnd)
         # TODO: err handling here, not very robust way to detect SP window
         if "PyCharm" in title:
             _pycharm_handle = hwnd
@@ -51,7 +53,7 @@ def get_app_handles() -> Tuple[Optional[int], Optional[int]]:
     return _spy_party_handle, _pycharm_handle
 
 
-def window__get_title(hwnd_handle):
+def window_get_title(hwnd_handle):
     length = ctypes.windll.user32.GetWindowTextLengthW(hwnd_handle)
     if length > 0:
         buff = ctypes.create_unicode_buffer(length + 1)
@@ -60,15 +62,44 @@ def window__get_title(hwnd_handle):
     return ""
 
 
+def reassemble(_game_index, timeline, games):
+    games[_game_index].timeline = timeline
+    coh_bool, coh_reasons = games[_game_index].is_timeline_coherent()
+    games[_game_index].repickle()
+    if not coh_bool:
+        print(games[_game_index], coh_reasons)
+        with open(PARSE_LOG, "a+") as parse_log:
+            parse_log.write(
+                f"incoherent timeline {games[_game_index].spy} vs. {games[_game_index].sniper} on {games[_game_index].venue} {games[_game_index].uuid} : {coh_reasons}\n"
+            )
+        pkl_loc = get_game_expected_pkl(games[_game_index].uuid)
+        pkl_name = os.path.split(pkl_loc)[1]
+        move(
+            LONG_FILE_HEADER + pkl_loc,
+            LONG_FILE_HEADER + os.path.join(PICKLE_ISOLATION, pkl_name),
+        )
+
+
+def combine_pieces(_game_index, timeline_pieces):
+    ordered_line_items = sorted(
+        timeline_pieces[_game_index].items(), key=lambda x: x[0]
+    )
+    this_events = [v for k, v in ordered_line_items]
+    flattened_lines = itertools.chain.from_iterable(this_events)
+    events = remove_overlap(flattened_lines)
+    timeline = Timeline(events)
+    timeline.calculate_elapsed_times()
+    return timeline
+
+
 def parse_timeline_parallel(games: List[Game]):
 
     if not games:
         return
 
-    spy_party_handle, pycharm_handle = get_app_handles()
+    handles = get_app_handles()
 
     mutex = threading.Lock()
-    num_worker_threads = 2
     queue = Queue()
     threads = []
 
@@ -103,40 +134,17 @@ def parse_timeline_parallel(games: List[Game]):
 
             # all the pieces are present, finish the parse and pickle the game
             if last_seen and num_parsed_pieces == last_num:
-                ordered_line_items = sorted(
-                    timeline_pieces[_game_index].items(), key=lambda x: x[0]
-                )
-                this_events = [v for k, v in ordered_line_items]
-                flattened_lines = itertools.chain.from_iterable(this_events)
-                events = remove_overlap(flattened_lines)
-                timeline = Timeline(events)
-                timeline.calculate_elapsed_times()
+                timeline = combine_pieces(_game_index, timeline_pieces)
 
                 mutex.acquire()
                 try:
-                    games[_game_index].timeline = timeline
-                    coh_bool, coh_reasons = games[_game_index].is_timeline_coherent()
-                    games[_game_index].repickle()
-                    if not coh_bool:
-                        print(games[_game_index], coh_reasons)
-                        with open(PARSE_LOG, "a+") as parse_log:
-                            parse_log.write(
-                                f"incoherent timeline {games[_game_index].spy} vs. {games[_game_index].sniper} on {games[_game_index].venue} {games[_game_index].uuid} : {coh_reasons}\n"
-                            )
-
-                        pkl_loc = get_game_expected_pkl(games[_game_index].uuid)
-                        pkl_name = os.path.split(pkl_loc)[1]
-                        move(
-                            LONG_FILE_HEADER + pkl_loc,
-                            LONG_FILE_HEADER + os.path.join(PICKLE_ISOLATION, pkl_name),
-                        )
-
+                    reassemble(_game_index, timeline, games)
                 finally:
                     mutex.release()
 
             queue.task_done()
 
-    for _ in range(num_worker_threads):
+    for _ in range(NUM_WORKER_THREADS):
         thread = threading.Thread(target=screenshot_consumer)
         thread.start()
         threads.append(thread)
@@ -148,32 +156,34 @@ def parse_timeline_parallel(games: List[Game]):
         print(
             f"{game.spy} vs. {game.sniper} on {game.venue} [{game.uuid}, {game_index + 1}/{len(games)}]"
         )
-        for (number, screenshot, last) in get_screenshots(
-            spy_party_handle, pycharm_handle
-        ):
+        for (number, screenshot, last) in get_screenshots(*handles):
             queue.put((game_index, (number, screenshot, last)))
 
             if last and game_index != (len(games) - 1):
-                pyautogui.hotkey("ctrl", "n")
-                sleep(0.250)
-
-                if is_game_loaded(spy_party_handle, pycharm_handle):
-                    sleep(0.250)
-                    pyautogui.press("f11")
-
-                # Starting in "SpyParty v0.1.6729.0", the timeline does not open to the start,
-                # but it opens to the start of game instead, which means that a scroll up in needed
-                # to capture everything.
-                for _ in range(30):
-                    pyautogui.scroll(1)
-                    sleep(0.02)
-
-                sleep(0.5)
+                load_next_game(handles)
 
     queue.join()
 
-    for _ in range(num_worker_threads):
+    for _ in range(NUM_WORKER_THREADS):
         queue.put((None, (None, None, None)))
 
     for thread in threads:
         thread.join()
+
+
+def load_next_game(handles):
+    pyautogui.hotkey("ctrl", "n")
+    sleep(0.250)
+
+    if is_game_loaded(*handles):
+        sleep(0.250)
+        pyautogui.press("f11")
+
+    # Starting in "SpyParty v0.1.6729.0", the timeline does not open to the start,
+    # but it opens to the start of game instead, which means that a scroll up in needed
+    # to capture everything.
+    for _ in range(30):
+        pyautogui.scroll(1)
+        sleep(0.02)
+
+    sleep(0.5)
