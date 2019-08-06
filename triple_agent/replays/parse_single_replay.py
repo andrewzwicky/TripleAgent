4cdbaa748bd67f6b3a64bf5229e1f5ce00ee8a3f
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-08-06 13:23:23 -0500

    First stab at splitting out coherency check
    Need tests for this as well.

--- triple_agent/replays/parse_single_replay.py
+++ triple_agent/replays/parse_single_replay.py
@@ -3,6 +3,8 @@ from typing import Optional, Dict, AnyStr
 
 from spyparty.ReplayParser import ReplayParser
 from triple_agent.utilities.game import Game, game_load_or_new
+from triple_agent.utilities.missions import convert_mission_set_to_enum
+from triple_agent.utilities.outcomes import WinType
 
 
 def get_replay_dict(replay_file: str) -> Optional[defaultdict]:
@@ -26,17 +28,23 @@ def parse_replay_dict_into_game(
         replay_dict["spy_displayname"],
         replay_dict["sniper_displayname"],
         replay_dict["level"],
-        replay_dict["result"].replace(" ", ""),
+        WinType[replay_dict["result"].replace(" ", "")],
         replay_dict["game_type"],
-        set(m.split(" ")[0] for m in replay_dict["selected_missions"]),
-        set(m.split(" ")[0] for m in replay_dict["picked_missions"]),
-        set(m.split(" ")[0] for m in replay_dict["completed_missions"]),
-        start_time=replay_dict["start_time"],
-        guest_count=replay_dict["guest_count"],
-        start_clock_seconds=replay_dict["start_clock_seconds"],
-        duration=replay_dict["duration"],
-        uuid=replay_dict["uuid"],
-        file=replay_file,
+        convert_mission_set_to_enum(
+            {m.split(" ")[0] for m in replay_dict["picked_missions"]}
+        ),
+        convert_mission_set_to_enum(
+            {m.split(" ")[0] for m in replay_dict["selected_missions"]}
+        ),
+        convert_mission_set_to_enum(
+            {m.split(" ")[0] for m in replay_dict["completed_missions"]}
+        ),
+        replay_dict["start_time"],
+        replay_dict["uuid"],
+        replay_file,
+        replay_dict["guest_count"],
+        replay_dict["start_clock_seconds"],
+        replay_dict["duration"],
         initial_pickle=False,
         **kwargs,
     )
