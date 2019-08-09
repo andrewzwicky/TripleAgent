59322a09b7b076a5547264e172b77064093c830b
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-08-09 00:21:36 -0500

    Create test_timeline_coherent.py
    Checking in start of coherency tests

---
+++ triple_agent/tests/test_timeline_coherent.py
@@ -0,0 +1,19 @@
+# import pytest
+#
+# from triple_agent.classes.game import Game
+# from triple_agent.classes.timeline import TimelineEvent, Timeline, TimelineCategory
+# from triple_agent.classes.outcomes import WinType
+#
+# TIMELINE_COHERENT_TESTS = [
+#     (),
+# ]
+#
+#
+# def test_timeline_coherent(timeline, expected_coherent):
+#     game = Game(
+#         "spy",
+#         "sniper",
+#         "venue",
+#         "MissionsWin",
+#         "a2/3",
+#     )
