16081d92fe2f1e8bb493756c357f05b3b68c6f72
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-08-11 21:06:04 -0500

    Update mission_completes.py
    Refactor to reduce complexity

--- triple_agent/reports/specific/mission_completes.py
+++ triple_agent/reports/specific/mission_completes.py
@@ -1,5 +1,5 @@
 from collections import defaultdict, Counter
-from typing import List, Dict
+from typing import List
 
 from triple_agent.reports.generation.report_utilities import create_bar_plot
 from triple_agent.reports.generation.generic_query import query
@@ -7,9 +7,7 @@ from triple_agent.classes.game import Game
 from triple_agent.classes.missions import (
     MISSIONS_ENUM_TO_COLOR,
     MISSION_PLOT_ORDER,
-    MISSION_LETTERS_TO_ENUM,
     Missions,
-    print_complete_string,
 )
 
 
@@ -18,6 +16,18 @@ def _mission_completes(games: List[Game], data_dictionary: Counter):
         for mission in Missions:
             if mission & game.completed_missions:
                 data_dictionary[mission] += 1
+
+
+# not a generic query_function, special case
+def _mission_completes_details(games: List[Game], data_dictionary: defaultdict):
+    for game in games:
+        # TODO: doesn't really for for pick modes
+        for mission in Missions:
+            if mission & game.selected_missions and mission != Missions.Zero:
+                if mission & game.completed_missions:
+                    data_dictionary[mission][True] += 1
+                else:
+                    data_dictionary[mission][False] += 1
 
 
 def mission_completion_query(games: List[Game], title: str, **kwargs):
@@ -31,71 +41,48 @@ def mission_completion_query(games: List[Game], title: str, **kwargs):
     query(games, title, _mission_completes, **default_kwargs)
 
 
-def mission_completion(
-    games: List[Game], title: str, venue_mode_dict: Dict[str, str] = None
-):
-    overall_key = "Overall"
-    times_enabled = defaultdict(Counter)
-    times_performed = defaultdict(Counter)
-    games_count_by_venue = Counter()
+def mission_completion(games: List[Game], title: str):
+    """
+    This report is slightly different, because the desire is to sort the data
+    by mission, rather than by an attribute of the game itself.  This means
+    it doesn't exactly fit into the existing report workflow
+    """
 
-    for game in games:
-        if venue_mode_dict is None or (
-            game.venue in venue_mode_dict.keys()
-            and game.game_type == venue_mode_dict[game.venue]
-        ):
+    total_games = len(games)
 
-            games_count_by_venue[game.venue] += 1
-            games_count_by_venue[overall_key] += 1
+    data_dictionary = defaultdict(Counter)
 
-            times_enabled[game.venue] += Counter(
-                print_complete_string(game.picked_missions).replace(" ", "")
-            )
-            times_enabled[overall_key] += Counter(
-                print_complete_string(game.picked_missions).replace(" ", "")
-            )
+    _mission_completes_details(games, data_dictionary)
 
-            times_performed[game.venue] += Counter(
-                print_complete_string(game.completed_missions).replace(" ", "")
-            )
-            times_performed[overall_key] += Counter(
-                print_complete_string(game.completed_missions).replace(" ", "")
-            )
+    complete_labels = []
+    incomplete_labels = []
 
-    for venue, total_games in sorted(
-        games_count_by_venue.items(), key=lambda x: (x[0] != overall_key, x[0])
-    ):
+    complete_percentage = []
+    incomplete_percentage = []
 
-        percs = []
-        uncompleted = []
-        percs_labels = []
-        uncompleted_labels = []
+    for mission in Missions:
+        if mission != Missions.Zero:
+            completed = data_dictionary[mission][True]
+            available = data_dictionary[mission][False] + completed
+            perc_avail = 0 if total_games == 0 else (available / total_games)
+            perc_complete_bar = 0 if available == 0 else (completed / total_games)
+            perc_complete_label = 0 if available == 0 else (completed / available)
 
-        for letter, _ in MISSION_LETTERS_TO_ENUM.items():
-            perf = times_performed[venue][letter]
-            avail = times_enabled[venue][letter]
-            perc_avail = 0 if total_games == 0 else (avail / total_games)
-            perc_complete_bar = 0 if avail == 0 else (perf / total_games)
-            perc_complete_label = 0 if avail == 0 else (perf / avail)
-
-            percs.append(perc_complete_bar)
-            percs_labels.append(f"{perf:>3}/{avail:>3}\n{perc_complete_label:>5.1%}")
-
-            uncompleted.append(perc_avail - perc_complete_bar)
-            uncompleted_labels.append(
-                f"{avail:>3}/{total_games:>3}\n{perc_avail:>5.1%}"
+            complete_percentage.append(perc_complete_bar)
+            complete_labels.append(
+                f"{completed:>3}/{available:>3}\n{perc_complete_label:>5.1%}"
             )
 
-        if venue_mode_dict is not None and venue in venue_mode_dict.keys():
-            mode_str = " " + venue_mode_dict[venue]
-        else:
-            mode_str = ""
+            incomplete_percentage.append(perc_avail - perc_complete_bar)
+            incomplete_labels.append(
+                f"{available:>3}/{total_games:>3}\n{perc_avail:>5.1%}"
+            )
 
-        create_bar_plot(
-            f"{title} - {venue} {mode_str}",
-            [percs, uncompleted],
-            [m.name for m in MISSION_LETTERS_TO_ENUM.values()],
-            bar_labels=[percs_labels, uncompleted_labels],
-            colors=["xkcd:green", "xkcd:light grey"],
-            percentage=True,
-        )
+    create_bar_plot(
+        title,
+        [complete_percentage, incomplete_percentage],
+        [m.name for m in Missions if m is not Missions.Zero],
+        bar_labels=[complete_labels, incomplete_labels],
+        colors=["xkcd:green", "xkcd:light grey"],
+        percentage=True,
+    )
