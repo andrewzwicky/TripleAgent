00f59fd0cf63d73ead4d4c69b13d4201fd34cf1e
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-08-20 13:41:00 -0500

    Update test_common_sort_functions.py
    Reflect actual functionality

--- triple_agent/tests/test_common_sort_functions.py
+++ triple_agent/tests/test_common_sort_functions.py
@@ -1,20 +1,50 @@
+import pandas
 import pytest
 from triple_agent.reports.generation.common_sort_functions import (
     sort_by_sniper_wins,
     sort_by_spy_wins,
 )
-from triple_agent.reports.specific.game_outcomes import _categorize_outcomes
-from triple_agent.reports.generation.plot_utilities import create_data_dictionary
+
+from triple_agent.classes.outcomes import WinType
+
+SORT_CASES = [
+    (
+        sort_by_spy_wins,
+        (
+            "Test",
+            pandas.Series(
+                data=[0, 1, 3, 4],
+                index=[
+                    WinType.SpyShot,
+                    WinType.TimeOut,
+                    WinType.CivilianShot,
+                    WinType.MissionsWin,
+                ],
+            ),
+        ),
+        7,
+    ),
+    (
+        sort_by_sniper_wins,
+        (
+            "Test",
+            pandas.Series(
+                data=[0, 1, 3, 4],
+                index=[
+                    WinType.SpyShot,
+                    WinType.TimeOut,
+                    WinType.CivilianShot,
+                    WinType.MissionsWin,
+                ],
+            ),
+        ),
+        1,
+    ),
+]
 
 
 @pytest.mark.plotting
 @pytest.mark.quick
-def test_sort_by_wins(get_preparsed_timeline_games):
-    data_dictionary = create_data_dictionary(
-        get_preparsed_timeline_games, _categorize_outcomes, groupby=lambda g: g.sniper
-    )
-
-    assert sort_by_spy_wins(data_dictionary["zerotka"]) == 1
-    assert sort_by_spy_wins(data_dictionary["Calvin Schoolidge"]) == 0
-    assert sort_by_sniper_wins(data_dictionary["zerotka"]) == 3
-    assert sort_by_sniper_wins(data_dictionary["Calvin Schoolidge"]) == 4
+@pytest.mark.parametrize("sort_func, series_tuple, exp_value", SORT_CASES)
+def test_sort_by_wins(sort_func, series_tuple, exp_value):
+    assert sort_func(series_tuple) == exp_value
