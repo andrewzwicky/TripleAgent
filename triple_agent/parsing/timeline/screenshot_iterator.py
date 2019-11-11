e1fdb5c3d266fec594b8802178a836bca099c6e1
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-11-10 20:49:24 -0600

    Update  SpyParty version

--- triple_agent/parsing/timeline/screenshot_iterator.py
+++ triple_agent/parsing/timeline/screenshot_iterator.py
@@ -43,7 +43,7 @@ def get_app_handles() -> Tuple[Optional[int], Optional[int]]:
         # TODO: err handling here, not very robust way to detect SP window
         if "PyCharm" in title:
             pycharm_handle = hwnd
-        if "SpyParty v0.1.6729.0" in title:
+        if "SpyParty v0.1.6873.0" in title:
             spyparty_handle = hwnd
 
         return True
