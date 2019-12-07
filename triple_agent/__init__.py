261274abb7c427f3a7147e374debe8966344612d
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-12-07 17:05:03 -0600

    Update __init__.py
    Finally fixing my environment to not need this stuff

--- triple_agent/__init__.py
+++ triple_agent/__init__.py
@@ -1,11 +1,1 @@
 __version__ = "0.0.1"
-
-try:
-    # This is to account for the difference between my local account and the CI environment.
-    # If SpyPartyParse is installed and added to the python site-packages properly, there is no
-    # need for a separate sys.path.append.  But if it's a git repo, I need to add it to my path.
-    # This also means I won't accidentally check in changes to __init__ that I don't actually want to push.
-    # pylint: disable=import-error
-    import __append_spp_path__
-except ModuleNotFoundError:
-    pass
