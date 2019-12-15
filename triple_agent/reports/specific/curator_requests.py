dbbc1e335013271e813603b490107b46d7715a69
Andrew Zwicky <andrew.zwicky@gmail.com>
2019-12-15 13:26:41 -0600

    Update curator_requests.py
    black formatter

--- triple_agent/reports/specific/curator_requests.py
+++ triple_agent/reports/specific/curator_requests.py
@@ -4,8 +4,9 @@ from triple_agent.organization.extract_spectation_replays import (
 from triple_agent.classes.action_tests import ActionTest
 from triple_agent.classes.outcomes import WinType
 
+
 def curated_many_green_ats(r):
-    if r.venue != 'Balcony' and r.win_type in [WinType.TimeOut, WinType.MissionsWin]:
+    if r.venue != "Balcony" and r.win_type in [WinType.TimeOut, WinType.MissionsWin]:
         if r.timeline is not None:
             this_game_green_tests = 0
             for e in r.timeline:
@@ -17,22 +18,29 @@ def curated_many_green_ats(r):
 
     return False
 
+
 def cough_clank_crash(r):
-    if r.venue != 'Balcony' and r.win_type in [WinType.TimeOut, WinType.MissionsWin]:
-        if r.game_type.startswith('a4') or r.game_type.startswith('a5'):
+    if r.venue != "Balcony" and r.win_type in [WinType.TimeOut, WinType.MissionsWin]:
+        if r.game_type.startswith("a4") or r.game_type.startswith("a5"):
             if r.timeline is not None:
                 for e in r.timeline:
-                    if e.event in [
-                        # clank
-                        "dropped statue.",
-                        # cough
-                        "banana bread aborted.", "action test red: contact double agent",
-                        # crash
-                        "purloin guest list aborted."
-                    ] and (r.duration - e.elapsed_time) > 10:
+                    if (
+                        e.event
+                        in [
+                            # clank
+                            "dropped statue.",
+                            # cough
+                            "banana bread aborted.",
+                            "action test red: contact double agent",
+                            # crash
+                            "purloin guest list aborted.",
+                        ]
+                        and (r.duration - e.elapsed_time) > 10
+                    ):
                         return True
 
     return False
+
 
 if __name__ == "__main__":
     extract_spectate_replays(cough_clank_crash)
