5ab6c2f61dfb257644726cdcb6f662d8409cc8c9
Andrew Zwicky <andrew.zwicky@gmail.com>
2020-01-02 20:34:49 -0600

    Adding overtime category to the ones missing OT.

--- triple_agent/parsing/replay/get_parsed_replays.py
+++ triple_agent/parsing/replay/get_parsed_replays.py
@@ -4,13 +4,12 @@ from typing import List
 from triple_agent.constants.paths import REPLAY_PICKLE_FOLDER
 from triple_agent.classes.game import game_unpickle, Game
 
+def _yield_unpickled_games(pickle_folder):
+    for f in os.listdir(pickle_folder):
+        yield game_unpickle(os.path.join(pickle_folder, f))
 
 def get_parsed_replays(
     game_filter, pickle_folder: str = REPLAY_PICKLE_FOLDER
 ) -> List[Game]:
-    unfiltered_game_list = [
-        game_unpickle(os.path.join(pickle_folder, f)) for f in os.listdir(pickle_folder)
-    ]
-    filtered_game_list = list(filter(game_filter, unfiltered_game_list))
 
-    return filtered_game_list
+    return list(filter(game_filter, _yield_unpickled_games(pickle_folder)))
