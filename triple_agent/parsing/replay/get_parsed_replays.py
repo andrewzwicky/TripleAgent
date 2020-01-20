7342df28ba07e9cd707a68b4e052d76a49147428
Andrew Zwicky <andrew.zwicky@gmail.com>
2020-01-20 16:47:36 -0600

    Update get_parsed_replays.py
    Reverting parallel processing, was causing test issues.

--- triple_agent/parsing/replay/get_parsed_replays.py
+++ triple_agent/parsing/replay/get_parsed_replays.py
@@ -1,20 +1,17 @@
+import os
 from typing import List
-from multiprocessing import Pool, cpu_count
-from pathlib import Path
 
 from triple_agent.constants.paths import REPLAY_PICKLE_FOLDER
 from triple_agent.classes.game import game_unpickle, Game
 
 
+def _yield_unpickled_games(pickle_folder):
+    for file in os.listdir(pickle_folder):
+        yield game_unpickle(os.path.join(pickle_folder, file))
+
+
 def get_parsed_replays(
     game_filter, pickle_folder: str = REPLAY_PICKLE_FOLDER
 ) -> List[Game]:
-    pool = Pool(processes=cpu_count() * 2)
 
-    try:
-        games = pool.map(game_unpickle, Path(pickle_folder).iterdir())
-    finally:
-        pool.close()
-        pool.join()
-
-    return list(filter(game_filter, games))
+    return list(filter(game_filter, _yield_unpickled_games(pickle_folder)))
