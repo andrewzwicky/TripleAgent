ab3a046bb9f606ce86816d80384bda2cac5837a6
Andrew Zwicky <andrew.zwicky@gmail.com>
2020-01-23 15:33:35 -0600

    Update ReplayParser.py
    Adding explanation and no cover for mock replay parser

--- triple_agent/mock/ReplayParser.py
+++ triple_agent/mock/ReplayParser.py
@@ -1,3 +1,6 @@
-class ReplayParser:
+# this class only exists to allow tools/IDEs to parse the
+# code without throwing a bunch of errors.  It's never actually
+# run.
+class ReplayParser:  # pragma: no cover
     def __init__(self, _):
         raise NotImplementedError
