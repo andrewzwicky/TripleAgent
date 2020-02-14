e41b27dc9770bf416289dea2c2135141c1a3f5a9
Andrew Zwicky <andrew.zwicky@gmail.com>
2020-02-14 15:09:21 -0600

    Vulture whitelist is now actually using referenced names and tested for import errors
    fixes #11

--- vulture_whitelist.py
+++ vulture_whitelist.py
@@ -1,12 +1,79 @@
+import pytesseract
+
+from triple_agent.classes.game import GameHandler
+from triple_agent.classes.outcomes import WinType
+from triple_agent.constants.colors import (
+    PlotColorsWong,
+    PlotColorsTolBright,
+    PlotColorsTolMuted,
+    PlotColorsSCL,
+)
+from triple_agent.constants.events import (
+    SCL5_DIVISIONS,
+    SCL5_PICK_MODES,
+    SCL6_VENUE_MODES,
+    select_sc19,
+    select_scl5,
+    select_scl5_regular_season,
+)
+from triple_agent.reports.specific.action_tests import (
+    action_test_percentages,
+    diff_action_test_percentages,
+)
+from triple_agent.reports.specific.banana_breads import (
+    all_banana_bread_percentages,
+    first_banana_bread_percentages,
+    banana_split,
+)
+from triple_agent.reports.specific.bug import bug_attempt_timings, bug_success_rate
+from triple_agent.reports.specific.character_selection import (
+    spy_selection,
+    amba_selection,
+    double_agent_selection,
+    st_selection,
+)
+from triple_agent.reports.specific.fingerprints import (
+    attempted_fingerprint_sources,
+    successful_fingerprint_sources,
+)
+from triple_agent.reports.specific.game_durations import game_durations
+from triple_agent.reports.specific.game_outcomes import game_outcomes
+from triple_agent.reports.specific.microfilm import at_or_direct_mf
+from triple_agent.reports.specific.mission_choices import mission_choices
+from triple_agent.reports.specific.mission_completes import (
+    mission_completion,
+    mission_completion_query,
+)
+from triple_agent.reports.specific.scl_set_scores import (
+    game_differential,
+    scl_set_scores_categorize,
+)
+from triple_agent.reports.specific.seduce import first_flirt_timing
+from triple_agent.reports.specific.stop_talks import stop_talk_in_game_percentage
+from triple_agent.reports.specific.time_adds import (
+    time_add_times,
+    time_add_times_per_game,
+)
+from triple_agent.reports.specific.lights import amba_lights, spy_lights
+from triple_agent.reports.external_reports.misc_reports.curator_requests import (
+    cough_clank_crash,
+    curated_many_green_ats,
+)
+from triple_agent.tests.conftest import auto_close_all_figures
+from triple_agent.organization.extract_spectation_replays import (
+    extract_spectate_replays,
+)
+
+
 # library internals
-_.tesseract_cmd  # unused attribute (triple_agent\parsing\timeline\parse_timeline.py)
-auto_close_all_figures  # unused function (triple_agent\tests\conftest.py)
-extract_spectate_replays  # unused function (triple_agent\organization\extract_spectation_replays.py)
+pytesseract.pytesseract.tesseract_cmd
+auto_close_all_figures
+extract_spectate_replays
 
 # jsonpickle methods
 GameHandler
-flatten
-restore
+GameHandler.flatten
+GameHandler.restore
 
 # colors
 PlotColorsWong
@@ -15,41 +82,40 @@ PlotColorsTolMuted
 PlotColorsSCL
 
 # notebook usage
-SniperWin  # unused variable (triple_agent\classes\outcomes.py)
-SCL5_PICK_MODES  # unused variable (triple_agent\constants\events.py)
-SCL5_DIVISIONS  # unused variable (triple_agent\constants\events.py)
-SCL6_VENUE_MODES  # unused variable (triple_agent\constants\events.py)
-select_scl5  # unused function (triple_agent\constants\events.py)
-select_scl5_regular_season  # unused function (triple_agent\constants\events.py)
-select_sc19  # unused function (triple_agent\constants\events.py)
-action_test_percentages  # unused function (triple_agent\reports\specific\action_tests.py)
-diff_action_test_percentages  # unused function (triple_agent\reports\specific\action_tests.py)
-all_banana_bread_percentages  # unused function (triple_agent\reports\specific\banana_breads.py)
-first_banana_bread_percentages  # unused function (triple_agent\reports\specific\banana_breads.py)
-banana_split  # unused function (triple_agent\reports\specific\banana_breads.py)
-bug_attempt_timings  # unused function (triple_agent\reports\specific\bug.py)
-bug_success_rate  # unused function (triple_agent\reports\specific\bug.py)
-spy_selection  # unused function (triple_agent\reports\specific\character_selection.py)
-st_selection  # unused function (triple_agent\reports\specific\character_selection.py)
-amba_selection  # unused function (triple_agent\reports\specific\character_selection.py)
-double_agent_selection  # unused function (triple_agent\reports\specific\character_selection.py)
-attempted_fingerprint_sources  # unused function (triple_agent\reports\specific\fingerprints.py)
-successful_fingerprint_sources  # unused function (triple_agent\reports\specific\fingerprints.py)
-game_durations  # unused function (triple_agent\reports\specific\game_durations.py)
-game_outcomes  # unused function (triple_agent\reports\specific\game_outcomes.py)
-at_or_direct_mf  # unused function (triple_agent\reports\specific\microfilm.py)
-microfilm_direction  # unused function (triple_agent\reports\specific\microfilm.py)
-mission_choices  # unused function (triple_agent\reports\specific\mission_choices.py)
-mission_completion_query  # unused function (triple_agent\reports\specific\mission_completes.py)
-mission_completion  # unused function (triple_agent\reports\specific\mission_completes.py)
-scl_set_scores_categorize  # unused function (triple_agent\reports\specific\scl_set_scores.py)
-game_differential  # unused function (triple_agent\reports\specific\scl_set_scores.py)
-first_flirt_timing  # unused function (triple_agent\reports\specific\seduce.py)
-stop_talk_in_game_percentage  # unused function (triple_agent\reports\specific\stop_talks.py)
-time_add_times_per_game  # unused function (triple_agent\reports\specific\time_adds.py)
-time_add_times  # unused function (triple_agent\reports\specific\time_adds.py)
-spy_lights # unused function (triple_agent\reports\specific\lights.py)
-amba_lights # unused function (triple_agent\reports\specific\lights.py)
+WinType.SniperWin
+SCL5_PICK_MODES
+SCL5_DIVISIONS
+SCL6_VENUE_MODES
+select_scl5
+select_scl5_regular_season
+select_sc19
+action_test_percentages
+diff_action_test_percentages
+all_banana_bread_percentages
+first_banana_bread_percentages
+banana_split
+bug_attempt_timings
+bug_success_rate
+spy_selection
+st_selection
+amba_selection
+double_agent_selection
+attempted_fingerprint_sources
+successful_fingerprint_sources
+game_durations
+game_outcomes
+at_or_direct_mf
+mission_choices
+mission_completion_query
+mission_completion
+scl_set_scores_categorize
+game_differential
+first_flirt_timing
+stop_talk_in_game_percentage
+time_add_times_per_game
+time_add_times
+spy_lights
+amba_lights
 
 # archive, but unused
 cough_clank_crash
