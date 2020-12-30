49cbbab1df68398d9f2ccf45dbb3e394655ae0c0
Andrew Zwicky <andrew.zwicky@gmail.com>
2020-12-29 19:42:29 -0600

    Update ipython_notebook.py
    Make changes to accomodate python 3.8 bugs

--- triple_agent/reports/generation/ipython_notebook.py
+++ triple_agent/reports/generation/ipython_notebook.py
@@ -1,8 +1,15 @@
+import sys
+import asyncio
+
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
 def execute_single_notebook(notebook_filename):
+    # See https://bugs.python.org/issue37373 :(
+    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     with open(notebook_filename, "r", encoding="utf-8") as file_in:
         notebook = nbformat.read(file_in, as_version=4)
 
