a0d2b9e68d1d48f39e7ed56db7ec95dcf52c7183
Andrew Zwicky <andrew.zwicky@gmail.com>
2021-01-23 15:42:36 -0600

    Update plot_utilities.py
    Remove .serialize sorting backup since all things that are serializable are sortable enums now

--- triple_agent/reports/generation/plot_utilities.py
+++ triple_agent/reports/generation/plot_utilities.py
@@ -140,10 +140,7 @@ def create_initial_data_frame(
     # sorting will provide determinism between runs
     # just converting set to list was creating slight
     # variations between the same runs
-    try:
-        stacks = sorted(list(data_parts))
-    except TypeError:
-        stacks = sorted(list(data_parts), key=lambda dp: dp.serialize())
+    stacks = sorted(list(data_parts))
 
     # Something needs to enumerate all the possibilities so the frame doesn't end up with NaN values in it.
     frame = pandas.DataFrame(
